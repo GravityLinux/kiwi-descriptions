99f6832a9d02727d1515502f427f9c8840dac48f
Davide Cavalca <dcavalca@fedoraproject.org>
2023-08-22 13:42:48 -0700

    Remove kiwi leftovers we don't need

---
+++ pre_disk_sync.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Remove kiwi leftovers we don't need
+# https://github.com/OSInside/kiwi/issues/2343#issuecomment-1663427508
+rm /boot/mbrid /config.bootoptions /config.partids
+
+exit 0
